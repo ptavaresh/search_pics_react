670d7a06db6c84cf18bd1b4ccb93dce13884f443
Pedro <ptavaresh@gmail.com>
2019-07-02 15:28:04 -0500

    Building a list of images

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
-
+import ImageList from './ImageList';
 
 class App extends React.Component {
   state = { images: [] };
@@ -19,6 +19,7 @@ class App extends React.Component {
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
         Images {this.state.images.length}
+        <ImageList images={this.state.images}/>
       </div>
     );
   }
